321fb70ca57869259547397bd1ebda694081ddb8
syed <syedibrahim.developer@gmail.com>
2021-06-11 00:57:48 +0530

    My Profile changes

--- lib/main.dart
+++ lib/main.dart
@@ -5,20 +5,92 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  final String title = 'Hello World';
+  const MyApp({Key? key}) : super(key: key);
+
   @override
-  Widget build(BuildContext context) => MaterialApp(
-        home: Scaffold(
-          backgroundColor: Colors.blueGrey,
-          appBar: AppBar(
-            title: Text('Iam Rich'),
-            backgroundColor: Colors.blueGrey[900],
-          ),
-          body: Center(
-            child: Image(
-              image: AssetImage('images/ruby.png'),
-            ),
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.deepOrangeAccent,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50,
+                backgroundColor: Colors.white,
+                backgroundImage: AssetImage('images/ruby.png'),
+              ),
+              Text(
+                'Syed Ibrahim',
+                style: TextStyle(
+                  fontSize: 40,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico',
+                ),
+              ),
+              Text(
+                'MOBILE DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'SourceSansPro',
+                  color: Colors.white,
+                  fontSize: 20,
+                  letterSpacing: 2,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+                width: 250,
+                child: Divider(
+                  color: Colors.orangeAccent,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 360,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.deepOrange,
+                  ),
+                  title: Text(
+                    '+91 99419 70049',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.deepOrange,
+                        fontFamily: 'SourceSansPro',
+                        letterSpacing: 1),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 360,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.deepOrange,
+                  ),
+                  title: Text(
+                    'syedibrahim.developer@gmail.com',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.deepOrange,
+                        fontFamily: 'SourceSansPro',
+                        letterSpacing: 1),
+                  ),
+                ),
+              )
+            ],
           ),
         ),
-      );
+      ),
+    );
+  }
 }
